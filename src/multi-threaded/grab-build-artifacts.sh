0189136e782e5314f6061dda2c40f0698bf10294
Bryon Baker <bryonbakeraus@gmail.com>
2023-05-17 12:38:45 +1000

    Fixed typo

--- src/multi-threaded/grab-build-artifacts.sh
+++ src/multi-threaded/grab-build-artifacts.sh
@@ -7,7 +7,7 @@ echo "Extracting build artifacts from image: $1:$2"
 
 # Run buildah unshare in a subshell and pass the command sequence 
 # into the subshell.
-# Then reate the target directory on the build host and copy the 
+# Then create the target directory on the build host and copy the 
 # files from the mounted image to the host filesystem.
 buildah unshare <<EOF
 container=\$(buildah from $1:$2)

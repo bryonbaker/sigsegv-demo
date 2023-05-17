693702119f1c7163b39c1ad18c89d2ea1e904309
Bryon Baker <bryonbakeraus@gmail.com>
2023-05-17 11:05:33 +1000

    parameterised grap script

--- src/multi-threaded/grab-build-artifacts.sh
+++ src/multi-threaded/grab-build-artifacts.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
+echo "Extracting build artifacts from image: $1"
+
 # Run buildah unshare in a subshell
 buildah unshare <<EOF
-container=\$(buildah from quay.io/bryonbaker/mt-sigsegv:latest)
+container=\$(buildah from $1)
 mnt=\$(buildah mount \$container)
 
 cp \$mnt/app/mt-sigsegv.map ./image-artifacts/

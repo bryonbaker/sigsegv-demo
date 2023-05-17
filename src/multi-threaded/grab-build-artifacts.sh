5df8602ca1bb4cc8c20a198dc4531f0dff39d52c
Bryon Baker <bryonbakeraus@gmail.com>
2023-05-17 11:12:19 +1000

    Parameterised again

--- src/multi-threaded/grab-build-artifacts.sh
+++ src/multi-threaded/grab-build-artifacts.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 
-echo "Extracting build artifacts from image: $1"
+echo "Extracting build artifacts from image: $1:$2"
 
 # Run buildah unshare in a subshell
 buildah unshare <<EOF
-container=\$(buildah from $1)
+container=\$(buildah from $1:$2)
 mnt=\$(buildah mount \$container)
 
-cp \$mnt/app/mt-sigsegv.map ./image-artifacts/
-cp \$mnt/app/mt-sigsegv ./image-artifacts/
+mkdir -p ./image-artifacts
+cp \$mnt/app/$1.map ./image-artifacts/
+cp \$mnt/app/$1 ./image-artifacts/
 exit
 EOF
